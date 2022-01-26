3f852cb73dff56b05fd0d99ad59cd1d02012527e
Nicco <bresslerniccolo@gmail.com>
2022-01-25 23:38:33 -0500

    Added results div and variables to keep score

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
     const choices = ["rock", "paper", "scissors"];
     const randomChoice = Math.floor(Math.random() * choices.length);
@@ -8,16 +11,22 @@ function playRound(playerSelection, computerSelection) {
     
     if ((playerSelection === "rock") && (computerSelection === "scissors")) {
         console.log("You win! Rock beats scissors.");
+        playerScore++;
     } else if ((playerSelection === "paper") && (computerSelection === "rock")) {
         console.log("You win! Paper beats rock.");
+        playerScore++;
     } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
         console.log("You win! Scissors beats paper.");
+        playerScore++;
     } else if ((computerSelection === "rock") && (playerSelection === "scissors")) {
         console.log("You lose! Rock beats scissors.");
+        computerScore++;
     } else if ((computerSelection === "paper") && (playerSelection === "rock")) {
         console.log("You lose! Paper beats rock.");
+        computerScore++;
     } else if ((computerSelection === "scissors") && (playerSelection === "paper")) {
         console.log("You lose! Scissors beats paper.");
+        computerScore++;
     } else if ((playerSelection) === (computerSelection)) {
         console.log("The game is a tie.");
     }
