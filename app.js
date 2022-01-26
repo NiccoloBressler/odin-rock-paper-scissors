170d5016027085492ff721a7cf310e1aaa9754a1
Nicco <bresslerniccolo@gmail.com>
2022-01-25 23:33:52 -0500

    Fixed bug where uppercase computerPlay was not allowing button to log result

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
 function computerPlay() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     const randomChoice = Math.floor(Math.random() * choices.length);
     return choices[randomChoice];
 }
 
-
 function playRound(playerSelection, computerSelection) {
     
     if ((playerSelection === "rock") && (computerSelection === "scissors")) {
-        return "You win! Rock beats scissors."
+        console.log("You win! Rock beats scissors.");
     } else if ((playerSelection === "paper") && (computerSelection === "rock")) {
-        return "You win! Paper beats rock."
+        console.log("You win! Paper beats rock.");
     } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-        return "You win! Scissors beats paper."
+        console.log("You win! Scissors beats paper.");
     } else if ((computerSelection === "rock") && (playerSelection === "scissors")) {
-        return "You lose! Rock beats scissors."
+        console.log("You lose! Rock beats scissors.");
     } else if ((computerSelection === "paper") && (playerSelection === "rock")) {
-        return "You lose! Paper beats rock."
+        console.log("You lose! Paper beats rock.");
     } else if ((computerSelection === "scissors") && (playerSelection === "paper")) {
-        return "You lose! Scissors beats paper."
+        console.log("You lose! Scissors beats paper.");
     } else if ((playerSelection) === (computerSelection)) {
-        return "The game is a tie."
+        console.log("The game is a tie.");
     }
 }
+
+const rock = document.getElementById('rock');
+rock.onclick = () => playRound('rock', computerPlay());
+
+const paper = document.getElementById('paper');
+paper.onclick = () => playRound('paper', computerPlay());
+
+const scissors = document.getElementById('scissors');
+scissors.onclick = () => playRound('scissors', computerPlay());
 
 function game() {
-    for (var i = 0; i < 5; i++) {
-        const playerInput = prompt("Enter your choice: ");
-        const playerSelection = playerInput.toLowerCase();
-        const computerSelection = computerPlay().toLowerCase();
-        console.log(playRound(playerSelection, computerSelection));
-    }
+    // for (var i = 0; i < 5; i++) {
+    //     const playerInput = prompt("Enter your choice: ");
+    //     const playerSelection = playerInput.toLowerCase();
+    //     const computerSelection = computerPlay().toLowerCase();
+    //     console.log(playRound(playerSelection, computerSelection));
+    // }
 }
 
-game();
+// game();
